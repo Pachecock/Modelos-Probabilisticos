49d862e891b36776edcd9ccc5bf70b5ee8474c87
Mayra B <mayra_vafer@hotmail.com>
2019-03-15 09:42:46 +0100

    graficas

--- prueba1/plot1.R
+++ prueba1/plot1.R
@@ -1,20 +1,42 @@
-datos <- read.table("ultimo.txt", header = TRUE)
+datos <- read.table("todos.txt", header = TRUE)
 
-optimizador<- as.factor(datos[,1])
-final <- as.factor(datos[,2])
+opt<- as.factor(datos[,1])
+fin <- as.factor(datos[,2])
 ocu <- as.factor(datos[,3])
 acc <- datos[,4]
-g <- aov(acc ~ optimizador + final + ocu + optimizador*final + optimizador*ocu +
-          final*ocu + optimizador*final*ocu, data = datos)
+datos$ocu = factor(datos$ocu)
+datos$fin = factor(datos$fin)
+head(datos)
+str(datos)
+plot.design(acc ~ ., data = datos)
+
+op <- par(mfrow = c(3, 1))
+with(datos, {
+  interaction.plot(opt, fin, acc)
+  interaction.plot(opt, ocu, acc)
+  interaction.plot(fin, ocu, acc)
+  }
+)
+par(op)
+fm <- aov(acc ~ opt + fin + ocu + opt*fin + opt*ocu +
+          fin*ocu + opt*fin*ocu, data = datos)
 summary(g)
-
-fml <- update(g, .~optimizador)
+fm <- update(g, . ~ . - opt:fin:ocu )
 summary(fml)
-       
-anova(g, fml)
-fin <- aov(acc~ optimizador, data = datos)
 
-summary(fin)
+fml <- update(fml, .~opt+fin+ocu)
+summary(fm2)
+
+anova(fm, fml)
+
+model.tables(fml, type = "effects")
+
+model.tables(fml, type = "means")
+
+fm <- aov(acc ~ opt+fin+ocu, data = datos)
+#fm <- aov(acc~ opt, data = datos)
+summary(fm)
+
 op <- par(mfrow = c(2, 2))
-plot(fin)
+plot(fm)
 par(op)
